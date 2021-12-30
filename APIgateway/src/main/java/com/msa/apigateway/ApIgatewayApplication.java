10ec434e58e9d5d465411462982de21a10beb393
sinaenjuni <y2k4388@gmail.com>
2021-12-30 15:28:23 +0900

    after final project evaluation

---
+++ APIgateway/src/main/java/com/msa/apigateway/ApIgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.msa.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApIgatewayApplication.class, args);
+    }
+
+}
---
+++ APIgateway/src/test/java/com/msa/apigateway/ApIgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
